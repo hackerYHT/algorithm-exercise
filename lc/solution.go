4d3a9afeff53ae18f13e125554b13c7b2289d6f7
yehaitao01 <2418170957@qq.com>
2024-03-15 23:02:19 +0800

    49. 字母异位词分组

--- lc/solution.go
+++ lc/solution.go
@@ -1,5 +1,7 @@
 package lc
 
+import sort2 "sort"
+
 type Algorithm interface {
 	groupAnagrams(strs []string) [][]string
 	longestConsecutive(nums []int) int
@@ -11,9 +13,35 @@ type MyImpl struct {
 }
 
 func (m MyImpl) groupAnagrams(strs []string) [][]string {
-	return nil
+	var sort func(str string) string
+	sort = func(str string) string {
+		s := []rune(str)
+		sort2.Slice(s, func(i, j int) bool {
+			return s[i] < s[j]
+		})
+		return string(s)
+	}
+	myMap := make(map[string][]string, 0)
+	for _, str := range strs {
+		s := sort(str)
+		_, ok := myMap[s]
+		if ok {
+			myMap[s] = append(myMap[s], str)
+		} else {
+			myMap[s] = append(myMap[s], []string{str}...)
+		}
+	}
+	res := make([][]string, 0)
+	for _, strings := range myMap {
+		ans := make([]string, 0)
+		for _, s := range strings {
+			ans = append(ans, s)
+		}
+		res = append(res, ans)
+	}
+	return res
 }
 
 func (m MyImpl) longestConsecutive(nums []int) int {
-	return 0
+
 }
