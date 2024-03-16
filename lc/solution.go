80640b67a5c4ed12001fc8f76044ec2f7c4c9086
yehaitao01 <2418170957@qq.com>
2024-03-16 10:21:09 +0800

    283. 移动零

--- lc/solution.go
+++ lc/solution.go
@@ -68,3 +68,15 @@ func (m MyImpl) longestConsecutive(nums []int) int {
 	}
 	return res
 }
+func (m MyImpl) moveZeroes(nums []int) {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			for j := i + 1; j < len(nums); j++ {
+				if nums[j] != 0 {
+					nums[i], nums[j] = nums[j], nums[i]
+					break
+				}
+			}
+		}
+	}
+}
