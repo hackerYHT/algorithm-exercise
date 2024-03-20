3123e41c418e26b92cd130ff7b75a62f76b95af2
yehaitao01 <2418170957@qq.com>
2024-03-21 02:02:05 +0800

    web项目初始化

--- server/service/lc/lc_solution.go
+++ server/service/lc/lc_solution.go
@@ -3,7 +3,6 @@ package lc
 import (
 	"github.com/hackerYHT/algorithm-exercise/server/model/lc"
 	"github.com/hackerYHT/algorithm-exercise/server/utils"
-	"reflect"
 )
 
 type SolutionService struct{}
@@ -13,17 +12,6 @@ type AlgorithmImpl struct {
 }
 
 func (s *ServiceGroup) Resolve(mb lc.MethodBody) any {
-	method := mb.Method
-	var impl utils.MyImpl
-	// 使用反射遍历方法
-	methodValue := reflect.ValueOf(&impl).Elem()
-	for i := 0; i < methodValue.NumMethod(); i++ {
-		methodName := methodValue.Type().Method(i).Name
-		if methodName == method {
-			// 找到匹配的方法，执行并返回结果
-			result := methodValue.Method(i).Call(nil)
-			return result[0].Interface()
-		}
-	}
-	return nil
+	var impl AlgorithmImpl
+	return impl.SubarraySum([]int{1, 2, 3}, 3)
 }
