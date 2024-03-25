c1101ea4f43c5a78673faf7b84b44e0335f12f33
yehaitao01 <2418170957@qq.com>
2024-03-25 23:09:59 +0800

    48. 旋转图像

--- server/utils/solution.go
+++ server/utils/solution.go
@@ -350,13 +350,14 @@ func (m MyImpl) SpiralOrder(matrix [][]int) []int {
 }
 
 func (m MyImpl) rotate(matrix [][]int) {
-	for i := 0; i < len(matrix)/2; i++ {
-		for j := 0; j < len(matrix[0])-j-1; j++ {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		for j := 0; j < (n+1)/2; j++ {
 			tmp := matrix[i][j]
-			matrix[i][j] = matrix[i][len(matrix[0])-j-1]
-			matrix[len(matrix)-i-1][j] = matrix[len(matrix)-i-1][len(matrix[0])-j-1]
-			matrix[len(matrix)-i-1][len(matrix[0])-j-1] = matrix[i][len(matrix[0])-j-1]
-			matrix[i][len(matrix[0])-j-1] = tmp
+			matrix[i][j] = matrix[n-1-j][i]
+			matrix[n-1-j][i] = matrix[n-1-i][n-1-j]
+			matrix[n-1-i][n-1-j] = matrix[j][n-1-i]
+			matrix[j][n-1-i] = tmp
 		}
 	}
 }
