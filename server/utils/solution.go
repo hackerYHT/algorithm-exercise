659d10c253d231cdbb0a1b158c66a20001d4766d
yehaitao01 <2418170957@qq.com>
2024-04-07 23:04:06 +0800

    516. 最长回文子序列

--- server/utils/solution.go
+++ server/utils/solution.go
@@ -635,3 +635,21 @@ func (m MyImpl) countSubstrings(s string) int {
 	}
 	return res
 }
+
+func (m MyImpl) longestPalindromeSubseq(s string) int {
+	dp := make([][]int, len(s))
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(s))
+	}
+	for i := len(dp) - 1; i >= 0; i-- {
+		dp[i][i] = 1
+		for j := i + 1; j < len(dp); j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = int(math.Max(float64(dp[i+1][j]), float64(dp[i][j-1])))
+			}
+		}
+	}
+	return dp[0][len(dp)-1]
+}
