c17710d28146f8cd2d1ddb08163f8e1d8e8fa287
yehaitao01 <2418170957@qq.com>
2024-06-22 14:09:09 +0800

    55. 跳跃游戏

--- server/utils/solution.go
+++ server/utils/solution.go
@@ -1920,3 +1920,16 @@ func (m MyImpl) maxProfit(prices []int) int {
 	}
 	return res
 }
+
+func (m MyImpl) canJump(nums []int) bool {
+	l, r := 0, 0
+	nextSkipIndex := 0
+	for l <= r && r <= len(nums)-1 {
+		for i := l; i <= r; i++ {
+			nextSkipIndex = int(math.Max(float64(nextSkipIndex), float64(i+nums[i])))
+		}
+		l = r + 1
+		r = nextSkipIndex
+	}
+	return nextSkipIndex >= len(nums)-1
+}
