3037abd346a2f01d3f55748ed98e659db91c4125
yehaitao01 <2418170957@qq.com>
2024-07-17 00:25:27 +0800

    1143. 最长公共子序列 ac

--- server/utils/solution.go
+++ server/utils/solution.go
@@ -2238,3 +2238,21 @@ func (m MyImpl) longestPalindrome(s string) string {
 	}
 	return res
 }
+
+func (m MyImpl) longestCommonSubsequence(text1 string, text2 string) int {
+	dp := make([][]int, len(text1)+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(text2)+1)
+	}
+	for i := 1; i < len(dp); i++ {
+		for j := 1; j < len(dp[0]); j++ {
+			max := int(math.Max(math.Max(float64(dp[i-1][j-1]), float64(dp[i-1][j])), float64(dp[i][j-1])))
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max
+			}
+		}
+	}
+	return dp[len(dp)-1][len(dp[0])-1]
+}
