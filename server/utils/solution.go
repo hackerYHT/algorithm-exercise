ebce4d10e6963498c4768e57e09fcacaee8c5d50
yehaitao01 <2418170957@qq.com>
2024-07-16 23:48:57 +0800

    5. 最长回文子串 ac

--- server/utils/solution.go
+++ server/utils/solution.go
@@ -2213,3 +2213,28 @@ func (m MyImpl) minPathSum(grid [][]int) int {
 	}
 	return dp[len(dp)-1][len(dp[0])-1]
 }
+func (m MyImpl) longestPalindrome(s string) string {
+	dp := make([][]bool, len(s))
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]bool, len(s))
+	}
+	res := s[:1]
+	dp[0][0] = true
+	for j := 1; j < len(dp); j++ {
+		for i := 0; i < j; i++ {
+			if s[i] == s[j] {
+				if j-i < 3 {
+					dp[i][j] = true
+				} else {
+					dp[i][j] = dp[i+1][j-1]
+				}
+			} else {
+				dp[i][j] = false
+			}
+			if dp[i][j] && j-i+1 > len(res) {
+				res = s[i : j+1]
+			}
+		}
+	}
+	return res
+}
