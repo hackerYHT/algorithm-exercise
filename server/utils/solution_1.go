7b11abbf6820fca403cd6618e6c67f02fe88e341
yehaitao01 <2418170957@qq.com>
2024-08-06 00:34:04 +0800

    72. 编辑距离 ac

--- server/utils/solution_1.go
+++ server/utils/solution_1.go
@@ -50,3 +50,24 @@ func (m MyImplOne) maxProfit(prices []int) int {
 func (m MyImpl) longestValidParentheses(s string) int {
 	return 1
 }
+
+func (m MyImplOne) minDistance(word1 string, word2 string) int {
+	dp := make([][]int, len(word1)+1)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(word2)+1)
+		dp[i][0] = i
+	}
+	for i := 0; i < len(dp[0]); i++ {
+		dp[0][i] = i
+	}
+	for i := 1; i < len(dp); i++ {
+		for j := 1; j < len(dp[0]); j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = int(math.Min(math.Min(float64(dp[i-1][j]), float64(dp[i][j-1])), float64(dp[i-1][j-1]))) + 1
+			}
+		}
+	}
+	return dp[len(dp)-1][len(dp[0])-1]
+}
