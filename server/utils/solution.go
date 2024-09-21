5ca168946137a5380b37469ac1a9e51b9db3f02f
yehaitao01 <2418170957@qq.com>
2024-09-21 17:38:51 +0800

    628. 三个数的最大乘积 ac

--- server/utils/solution.go
+++ server/utils/solution.go
@@ -2658,3 +2658,7 @@ func (m MyImpl) myAtoi(s string) int {
 	}
 	return res * sign
 }
+func (m MyImpl) maximumProduct(nums []int) int {
+	sort.Ints(nums)
+	return int(math.Max(float64(nums[0]*nums[1]*nums[len(nums)-1]), float64(nums[len(nums)-1]*nums[len(nums)-2]*nums[len(nums)-3])))
+}
