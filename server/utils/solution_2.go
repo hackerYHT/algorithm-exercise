f99db78d00ab684d361f0567db2cd5796510b526
yehaitao01 <2418170957@qq.com>
2024-10-17 00:01:35 +0800

    54. 螺旋矩阵 ac

--- server/utils/solution_2.go
+++ server/utils/solution_2.go
@@ -181,5 +181,37 @@ func (m MyImplTwo) trap(height []int) int {
 	return res
 }
 func (m MyImplTwo) spiralOrder(matrix [][]int) []int {
-
+	res := make([]int, 0)
+	l, r, t, b := 0, len(matrix[0])-1, 0, len(matrix)-1
+	for l <= r && t <= b {
+		for i := l; i <= r; i++ {
+			res = append(res, matrix[t][i])
+		}
+		t++
+		if t > b {
+			break
+		}
+		for i := t; i <= b; i++ {
+			res = append(res, matrix[i][r])
+		}
+		r--
+		if l > r {
+			break
+		}
+		for i := r; i >= l; i-- {
+			res = append(res, matrix[b][i])
+		}
+		b--
+		if t > b {
+			break
+		}
+		for i := b; i >= t; i-- {
+			res = append(res, matrix[i][l])
+		}
+		l++
+		if l > r {
+			break
+		}
+	}
+	return res
 }
