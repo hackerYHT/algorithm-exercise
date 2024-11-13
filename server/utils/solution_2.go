1849088b82b58ba2be15a94cddb505163a9408b9
yehaitao01 <2418170957@qq.com>
2024-11-13 10:22:12 +0800

    165. 比较版本号 未ac

--- server/utils/solution_2.go
+++ server/utils/solution_2.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"math"
 	"sort"
-	"strconv"
 	"strings"
 )
 
@@ -632,6 +631,9 @@ func (m MyImplTwo) mySqrt(x int) int {
 	return l - 1
 }
 func (m MyImplTwo) compareVersion(version1 string, version2 string) int {
+	//1.001, 1.01
+	//1.10, 1.01
+	//1.0.0.0, 1.0
 	v1 := strings.Split(version1, ",")
 	v2 := strings.Split(version2, ",")
 	if len(v1) < len(v2) {
@@ -647,26 +649,18 @@ func (m MyImplTwo) compareVersion(version1 string, version2 string) int {
 		}
 	}
 	for i := 0; i < len(v1); i++ {
-		a, b := 0, 0
 		// 去掉先导0
 		for j := 0; j < len(v1[i])-1; j++ {
 			if v1[i][j] != '0' {
-				a, _ = strconv.Atoi(v1[i][j:])
+				v1[i] = v1[i][j:]
 				break
 			}
 		}
 		for j := 0; j < len(v2[i])-1; j++ {
 			if v2[i][j] != '0' {
-				b, _ = strconv.Atoi(v2[i][j:])
+				v2[i] = v2[i][j:]
 				break
 			}
-		}
-		if a < b {
-			return -1
-		} else if a > b {
-			return 1
-		} else {
-			return 0
 		}
 	}
 	return 0
