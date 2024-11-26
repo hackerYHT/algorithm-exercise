7706d1908eebe67406804f3d3d7fe2d0a6fc3930
yehaitao01 <2418170957@qq.com>
2024-11-26 23:20:47 +0800

    300. 最长递增子序列 ac

--- server/utils/solution_2.go
+++ server/utils/solution_2.go
@@ -769,3 +769,20 @@ func (m MyImplTwo) smallestDifference(a []int, b []int) int {
 	}
 	return res
 }
+
+func (m MyImplTwo) longestValidParentheses(s string) int {
+	res := 0
+	cnt := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			cnt++
+		} else {
+			if cnt > 0 {
+				cnt--
+				res++
+			}
+		}
+
+	}
+	return res * 2
+}
