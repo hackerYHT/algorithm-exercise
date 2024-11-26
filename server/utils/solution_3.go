3a0527f5060db6b9f32996291845fa9e48747a00
yehaitao01 <2418170957@qq.com>
2024-11-26 09:46:32 +0800

    micro soft 矩阵的左上角到右下角的所有路径数量 未ac

--- server/utils/solution_3.go
+++ server/utils/solution_3.go
@@ -37,3 +37,19 @@ func (m MyImplThree) getIntersectionNode(headA, headB *ListNode) *ListNode {
 	}
 	return A
 }
+func (im MyImplThree) numWays(n, m int) int {
+	dp := make([][]int, n)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, m)
+		dp[i][0] = 1
+	}
+	for i := 0; i < len(dp[0]); i++ {
+		dp[0][i] = 1
+	}
+	for i := 1; i < len(dp); i++ {
+		for j := 1; j < len(dp[0]); j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[n-1][m-1]
+}
