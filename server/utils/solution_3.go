70e70146fbe5f9b62c004b4c52d0630652af89b6
yehaitao01 <2418170957@qq.com>
2024-12-07 18:37:41 +0800

    4. 寻找两个正序数组的中位数 ac

--- server/utils/solution_3.go
+++ server/utils/solution_3.go
@@ -280,5 +280,34 @@ func (m MyImplThree) generateParenthesis(n int) []string {
 	return res
 }
 func (m MyImplThree) findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-
+	getTheKthNum := func(k int) float64 {
+		i1, i2 := 0, 0
+		for {
+			if i1 == len(nums1) {
+				return float64(nums2[i2+k-1])
+			}
+			if i2 == len(nums2) {
+				return float64(nums1[i1+k-1])
+			}
+			if k == 1 {
+				return math.Min(float64(nums1[i1]), float64(nums2[i2]))
+			}
+			half := k / 2
+			newi1 := int(math.Min(float64(i1+half), float64(len(nums1)))) - 1
+			newi2 := int(math.Min(float64(i2+half), float64(len(nums2)))) - 1
+			if nums1[newi1] <= nums2[newi2] {
+				k = k - (newi1 - i1 + 1)
+				i1 = newi1 + 1
+			} else {
+				k = k - (newi2 - i2 + 1)
+				i2 = newi2 + 1
+			}
+		}
+	}
+	len := len(nums1) + len(nums2)
+	if len%2 == 0 {
+		return (getTheKthNum(len/2+1) + getTheKthNum(len/2)) / 2
+	} else {
+		return getTheKthNum(len/2 + 1)
+	}
 }
