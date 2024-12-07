438d94971dfcaedd825e6c9467d708bfdc2ab90f
yehaitao01 <2418170957@qq.com>
2024-12-07 19:31:16 +0800

    48. 旋转图像 ac

--- server/utils/solution_3.go
+++ server/utils/solution_3.go
@@ -311,3 +311,16 @@ func (m MyImplThree) findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 		return getTheKthNum(len/2 + 1)
 	}
 }
+
+func (m MyImplThree) rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		for j := 0; j < (n+1)/2; j++ {
+			tmp := matrix[i][j]
+			matrix[i][j] = matrix[n-1-j][i]
+			matrix[n-1-j][i] = matrix[n-1-i][n-1-j]
+			matrix[n-1-i][n-1-j] = matrix[j][n-1-i]
+			matrix[j][n-1-i] = tmp
+		}
+	}
+}
